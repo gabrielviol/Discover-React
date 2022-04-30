9c201ab65afd75cce8660a044013d58eea805e83
Gabriel V Oliveira <gabrielviol00@hotmail.com>
2022-04-30 16:27:12 -0300

    create components

--- reactapp/src/pages/Home/index.jsx
+++ reactapp/src/pages/Home/index.jsx
@@ -1,6 +1,6 @@
 import './styles.css'
 
-
+import { Card } from '../../components/Card';
 
 export function Home() {
 
@@ -9,6 +9,10 @@ export function Home() {
       <h1>Lista de Presença</h1>
       <input type="text" placeholder="Digite o nome..."/>
       <button type="button">Adicionar</button>
+
+      <Card />
+      <Card />
+      <Card />
     </div>
   )
 }
