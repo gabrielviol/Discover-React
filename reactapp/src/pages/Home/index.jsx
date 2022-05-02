904b048bdbfaa5ae041ea78215fca9e0578300d6
Gabriel V Oliveira <gabrielviol00@hotmail.com>
2022-05-02 13:19:11 -0300

    using useState

--- reactapp/src/pages/Home/index.jsx
+++ reactapp/src/pages/Home/index.jsx
@@ -1,13 +1,24 @@
+import React, { useState } from 'react';
 import './styles.css'
 
 import { Card } from '../../components/Card';
 
 export function Home() {
 
+  const [studentName, setStudentName] = useState('Gabriel');
+
+  function handleNameChange(name){
+    studentName = name;
+  }
+
   return (
     <div className="container">
-      <h1>Lista de Presença</h1>
-      <input type="text" placeholder="Digite o nome..."/>
+      <h1>Nome: { studentName }</h1>
+      <input 
+        type="text" 
+        placeholder="Digite o nome..."
+        onChange={ e => setStudentName(e.target.value) }
+      />
       <button type="button">Adicionar</button>
 
       <Card name="Gabriel" time="10:50:25"/>
